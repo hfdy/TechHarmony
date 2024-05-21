203425914723883144036c6ab746e3d2aea431fd
lidong <lidong@nancal.com>
2024-05-21 17:20:36 +0800

    feat:增加首页接口和逻辑

--- library_base/src/main/ets/constants/ResourceFilePathConfig.ts
+++ library_base/src/main/ets/constants/ResourceFilePathConfig.ts
@@ -1,11 +1,1 @@
-export const HTTPS_DOMAIN:string='https://lshj.northking.net';
-export const HTTP_DOMAIN:string='http://lshj.northking.net';
-export const API_DOMAIN:string='http://api.edm.dev.northking.net/';
-export const APPS_DOMAIN:string='http://apps.mgateway.dev.northking.net/';
-export const PROTOCOL_FILE:string='file://';
-
-export const H5_ZIP_FOLD:string='/h5';
-export const RAW_ZIP_FILE:string='rawfile/dist.zip';
-export const MAIN_PATH:string='/pmb_index/index.html';
-export const APP_ID:string='1732329691023257601';
-
+export const API_DOMAIN:string='https://www.wanandroid.com/';
